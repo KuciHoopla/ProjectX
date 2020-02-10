e52775370237f9c856d42d9c5ed4a3d4a315909a
KuciHoopla <jan.kucera@me.com>
2020-02-10 10:56:32 +0100

    02101056

--- RPA/creators/variables/variables.py
+++ RPA/creators/variables/variables.py
@@ -6,7 +6,7 @@ date = datetime.now(timezone.utc)
 date = date.strftime('%Y-%m-%d-%H-%M-%S')
 
 
-resources_folder = "C:\\Users\\JanKucera\\PycharmProjects\\ProjectX\\RPA\\creators\\resources\\"
+resources_folder = "C:\\Users\\JanKucera\\PycharmProjects\\Project\\RPA\\creators\\resources\\"
 
 photos_folder = f'{resources_folder}\\photos'
 
